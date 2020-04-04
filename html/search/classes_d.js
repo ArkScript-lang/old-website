7e26b2926a398b9212ca46be6f65b34eff10c05f
SuperFola <lexplt.dev@gmail.com>
2020-04-04 11:34:23 +0200

    renaming html to impl, adding a config.yml (jekyll theme: minimal)

--- html/search/classes_d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['usertype',['UserType',['../class_ark_1_1_user_type.html',1,'Ark']]]
-];
--- html/search/classes_f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zerodivisionerror',['ZeroDivisionError',['../class_ark_1_1_zero_division_error.html',1,'Ark']]]
-];
